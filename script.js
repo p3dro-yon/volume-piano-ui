ee38691ee89243b13962e4958c51d22999f2d095
y0nakk <162389508+p3dro-yon@users.noreply.github.com>
2025-05-02 16:01:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentVolume = 50;
+const volumeDisplay = document.getElementById("volume-display");
+
+const noteRates = {
+  "C": 1.00,
+  "C#": 1.06,
+  "D": 1.12,
+  "D#": 1.19,
+  "E": 1.26,
+  "F": 1.33,
+  "F#": 1.41,
+  "G": 1.50,
+  "G#": 1.59,
+  "A": 1.68,
+  "A#": 1.78,
+  "B": 1.89
+};
+
+document.querySelectorAll(".key").forEach((key) => {
+  key.addEventListener("click", () => {
+    const note = key.dataset.note;
+    const vol = parseInt(key.dataset.volume);
+
+    currentVolume = vol;
+    volumeDisplay.textContent = `volume: ${currentVolume}%`;
+
+    const audio = new Audio("piano-c4.mp3");
+    audio.volume = currentVolume / 100;
+    audio.playbackRate = noteRates[note] || 1.0;
+    audio.preservesPitch = false;
+
+    audio.addEventListener("canplaythrough", () => {
+      audio.play();
+    });
+  });
+});
